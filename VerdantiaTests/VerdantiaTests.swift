830cc6a3d175e3cd85171d29c66d8c5740814e22
Jake Gibbons <jake_gibbons@live.co.uk>
2025-07-28 21:42:36 +0100

    Initial Commit

---
+++ VerdantiaTests/VerdantiaTests.swift
@@ -0,0 +1,17 @@
+//
+//  VerdantiaTests.swift
+//  VerdantiaTests
+//
+//  Created by Jake Gibbons on 28/07/2025.
+//
+
+import Testing
+@testable import Verdantia
+
+struct VerdantiaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
